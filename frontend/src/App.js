5a1bd264bcd3eab47d3d756f55f59c37d6b77b5c
Meghana Pothukuchi <meghana.pothukuchi@gmail.com>
2024-03-31 18:06:28 -0500

    updatd script and UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -63,9 +63,24 @@ const CARRIERS = {
   6: 'YRC',
 }
 
-const NUM_STATES = 48
+// const NUM_STATES = 48
 const NUM_CARRIERS = 7
-const NUM_PARAMS = 3
+// const NUM_PARAMS = 3
+
+function toFixed(num, fixed) {
+  var re = new RegExp('^-?\\d+(?:\.\\d{0,' + (fixed || -1) + '})?');
+  return num.toString().match(re)[0];
+}
+
+function customSort(a, b) {
+  if (a.score === -1 && b.score !== -1) {
+    return 1;
+  } else if (a.score !== -1 && b.score === -1) {
+    return -1;
+  } else {
+    return a.score - b.score;
+  }
+}
 
 function App() {
   const [state, setState] = useState('')
@@ -81,74 +96,161 @@ function App() {
   }
 
   const onSubmit = () => {
-    console.log('submit')
     let score = calculateScore(state.toUpperCase(), weight);
     setResult(score)
+  }
+
+  const onClear = () => {
+    setState('');
+    setWeight(0.0);
+    setResult('')
   }
 
   function calculateScore(state, weight) {
     const stateIdx = STATES[state];
 
-    const accessorialsWeight = 1.0;
-    const claimsWeight = 1.0;
-    const freightCostWeight = 1.0;
-
     let res = {
-      0: -1,
-      1: -1,
-      2: -1,
-      3: -1,
-      4: -1,
-      5: -1,
-      6: -1,
+      0: {
+        score: -1,
+        warnings: []
+      },
+      1: {
+        score: -1,
+        warnings: []
+      },
+      2: {
+        score: -1,
+        warnings: []
+      },
+      3: {
+        score: -1,
+        warnings: []
+      },
+      4: {
+        score: -1,
+        warnings: []
+      },
+      5: {
+        score: -1,
+        warnings: []
+      },
+      6: {
+        score: -1,
+        warnings: []
+      },
     }
 
     for (let carrier = 0; carrier < NUM_CARRIERS; carrier++) {
       const accessorialsScore = scores[stateIdx][carrier][0] === -1 ? 0 : scores[stateIdx][carrier][0];
-      const claimsScore = scores[stateIdx][carrier][1] === -1 ? 0 : scores[stateIdx][carrier][1];
+      // const claimsScore = scores[stateIdx][carrier][1] === -1 ? 0 : scores[stateIdx][carrier][1];
       const freightCostScore = scores[stateIdx][carrier][2];
 
       // Check for NaN freightCostScore
       if (freightCostScore !== -1.0) {
-        const score = (accessorialsScore * accessorialsWeight) +
-          (claimsScore * claimsWeight) +
-          (freightCostScore * weight * freightCostWeight);
-
-        res[carrier] = score;
+        const s = (accessorialsScore) + (freightCostScore * weight);
+        res[carrier].score = s;
       }
     }
 
-    return res;
+    Object.entries(res).map(([carrier, data]) => {
+      const claimsScore = scores[stateIdx][carrier][1] === -1 ? 0 : scores[stateIdx][carrier][1];
+      const transit_time_score = scores[stateIdx][carrier][3] === -1 ? 0 : scores[stateIdx][carrier][3];
+
+      if (data.score !== -1) {
+        if (claimsScore > 0) {
+          data.warnings.push(`${Math.floor(claimsScore)} claim(s) in the past`)
+        }
+
+        if (transit_time_score < 85) {
+          data.warnings.push(`Historically late deliveries.`)
+        }
+      }
+    })
+
+    // Sorts res by increasing score
+    const resArray = Object.entries(res).map(([index, value]) => ({ carrier: CARRIERS[index], ...value }));
+    resArray.sort(customSort);
+    const sortedRes = {};
+    resArray.forEach((obj, index) => {
+      sortedRes[index] = obj;
+    });
+
+    return sortedRes;
   }
 
 
   return (
-    <div className="App">
-      <input
-        type='text'
-        name='state'
-        onChange={onStateChange}
-        value={state}
-      />
-      <input
-        type='number'
-        step="0.0001"
-        name='weight'
-        onChange={onWeightChange}
-        value={weight}
-      />
-      <button type='submit' disabled={state === '' || weight == 0} onClick={onSubmit}>
-        Submit
-      </button>
+    <div>
+      <div className="container">
+        <div className="input-group mb-3">
+          <span
+            className="input-group-text"
+            id="inputGroup-sizing-default"
+          >
+            State Abbr. (not AL or HI)
+          </span>
+          <input
+            type="text"
+            name="state"
+            className="form-control"
+            onChange={onStateChange}
+            value={state}
+            aria-label="Sizing example input"
+            aria-describedby="inputGroup-sizing-default" />
+        </div>
+        <div className="input-group mb-3">
+          <span
+            className="input-group-text"
+            id="inputGroup-sizing-default"
+          >
+            Weight (lbs)
+          </span>
+          <input
+            type="number"
+            name="weight"
+            step="0.0001"
+            className="form-control"
+            onChange={onWeightChange}
+            value={weight}
+            aria-label="Sizing example input"
+            aria-describedby="inputGroup-sizing-default" />
+        </div>
+        <div className='rowCustom'>
+          <button type="button" className="btn btn-primary custom-btn" disabled={state === '' || weight === 0} onClick={onSubmit}>
+            Submit
+          </button>
+          <button type="button" className="btn btn-danger custom-btn" disabled={state === '' && weight === 0} onClick={onClear}>
+            Clear
+          </button>
+        </div>
+      </div>
       {result !== '' && (
-        <div>
-          {Object.entries(result).map(([carrier, score]) => (
-            <div key={carrier}>
-              Carrier: {CARRIERS[carrier]}, {score === -1 ? 'No existing freight/lb cost data' : `Score: ${score}`}
-            </div>
-          ))}
+        <div className='table-container'>
+          <table className="table table-striped">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Carrier</th>
+                <th scope="col">Score</th>
+                <th scope="col">Warnings</th>
+              </tr>
+            </thead>
+            <tbody>
+              {Object.entries(result).map(([idx, data]) => (
+                <tr key={idx}>
+                  <th scope="row">{idx}</th>
+                  <td>{data.carrier}</td>
+                  <td>{data.score === -1 ? 'No existing freight/lb cost data' : `${toFixed(data.score, 3)}`}</td>
+                  <td>
+                    {data.warnings.length > 0 ? data.warnings.join(", ") : 'N/A'}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       )}
+
     </div>
   );
 }
